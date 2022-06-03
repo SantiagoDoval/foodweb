b9b73eefc037e341c5ee040f6d2eaeb2848d4639
SantiagoDoval <danieldovals@gmail.com>
2022-06-02 21:28:07 -0500

    update 2.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,12 @@ import { MenuComponent } from './shared/menu/menu.component';
 import { HomeComponent } from './food/pages/home/home.component';
 import { CategoryComponent } from './food/pages/category/category.component';
 import { AboutComponent } from './food/pages/about/about.component';
+import { PopularComponent } from './food/pages/popular/popular.component';
+import { BannerComponent } from './food/pages/banner/banner.component';
+import { OrderComponent } from './food/pages/order/order.component';
+import { BlogsComponent } from './food/pages/blogs/blogs.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MenuFoodComponent } from './food/pages/menu-food/menu-food.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +19,13 @@ import { AboutComponent } from './food/pages/about/about.component';
     MenuComponent,
     HomeComponent,
     CategoryComponent,
-    AboutComponent
+    AboutComponent,
+    PopularComponent,
+    BannerComponent,
+    OrderComponent,
+    BlogsComponent,
+    MenuFoodComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule
