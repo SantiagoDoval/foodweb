0dca358236932f28e070fff4a805d87c6e43ce2b
SantiagoDoval <danieldovals@gmail.com>
2022-06-02 20:47:39 -0500

    update 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { HomeComponent } from './food/pages/home/home.component';
+import { CategoryComponent } from './food/pages/category/category.component';
+import { AboutComponent } from './food/pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    CategoryComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
